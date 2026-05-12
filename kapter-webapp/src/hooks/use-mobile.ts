980542d0ecf368920d97a94c6cb172dd493d31f8
sondoannam <sondoannam202@gmail.com>
2026-05-12 10:30:29 +0700

    refactor(webapp): :memo: temporary build fix

--- kapter-webapp/src/hooks/use-mobile.ts
+++ kapter-webapp/src/hooks/use-mobile.ts
@@ -8,9 +8,12 @@ export function useIsMobile() {
   React.useEffect(() => {
     const mql = window.matchMedia(`(max-width: ${MOBILE_BREAKPOINT - 1}px)`)
     const onChange = () => {
-      setIsMobile(window.innerWidth < MOBILE_BREAKPOINT)
+      requestAnimationFrame(() => {
+        setIsMobile(window.innerWidth < MOBILE_BREAKPOINT)
+      })
     }
     mql.addEventListener("change", onChange)
+    // eslint-disable-next-line react-hooks/set-state-in-effect
     setIsMobile(window.innerWidth < MOBILE_BREAKPOINT)
     return () => mql.removeEventListener("change", onChange)
   }, [])
