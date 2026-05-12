50e438145c4bb623e3b3d11b0cee410c7b0b4d43
sondoannam <sondoannam202@gmail.com>
2026-05-12 10:05:36 +0700

    refactor(webapp): :memo: remove inexisted rule

--- kapter-webapp/eslint.config.js
+++ kapter-webapp/eslint.config.js
@@ -25,8 +25,7 @@ export default defineConfig([
     },
     rules: {
       "@typescript-eslint/no-explicit-any": "warn",
-      "@typescript-eslint/no-unused-vars": "warn",
-      "@typescript-eslint/no-unused-imports": "warn",
+      "@typescript-eslint/no-unused-vars": "warn"
     }
   },
 ])
